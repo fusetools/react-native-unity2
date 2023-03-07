1d32ece07efb64cfd866d90c50420676bc062f9c
Morten Daniel Fornes <morten@fusetools.com>
2023-03-07 11:41:16 +0700

    example: update unpack script
    * Extract files to right directories
    * Delete old files before unpacking

--- example/unity/unpack.mjs
+++ example/unity/unpack.mjs
@@ -1,5 +1,6 @@
 import tar from "tar"
 import path from "path"
+import rimraf from "rimraf"
 import {fileURLToPath} from "url"
 
 const __filename = fileURLToPath(import.meta.url)
@@ -7,5 +8,10 @@ const __dirname = path.dirname(__filename)
 
 console.log("Unpacking Unity files in", __dirname)
 
-tar.x({C: `${__dirname}/android`, file: `${__dirname}/android.tgz`, sync: true})
-tar.x({C: `${__dirname}/ios`, file: `${__dirname}/ios.tgz`, sync: true})
+// Delete any old files
+rimraf.sync(`${__dirname}/android`)
+rimraf.sync(`${__dirname}/ios`)
+
+// Unpack tarballs
+tar.x({C: __dirname, file: `${__dirname}/android.tgz`, sync: true})
+tar.x({C: __dirname, file: `${__dirname}/ios.tgz`, sync: true})
