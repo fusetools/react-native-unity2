0ddb00601c79918fcf52d5e5dc08fbd2dd23cec8
Semen Lunin <leynelss@gmail.com>
2025-07-01 15:09:50 +0300

    android changes

--- android/src/main/java/no/fuse/rnunity/RNUnityModule.java
+++ android/src/main/java/no/fuse/rnunity/RNUnityModule.java
@@ -2,7 +2,10 @@
 package no.fuse.rnunity;
 
 import android.app.Activity;
+import android.graphics.PixelFormat;
 import android.util.Log;
+import android.os.Build;
+import android.view.ViewGroup;
 import android.view.WindowManager;
 
 import com.facebook.react.bridge.ReactApplicationContext;
@@ -13,12 +16,34 @@ import com.facebook.react.modules.core.DeviceEventManagerModule;
 
 import com.unity3d.player.UnityPlayer;
 
+import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
+import java.lang.reflect.InvocationTargetException;
+
 public class RNUnityModule extends ReactContextBaseJavaModule {
     static RNUnityModule instance;
+    private static UPlayer unityPlayer;
+    public static boolean _isUnityReady;
+    public static boolean _isUnityPaused;
+    public static boolean _fullScreen;
 
     // Called by C#
     public static RNUnityModule getInstance() {
         return instance;
+    }
+
+    public static UPlayer getPlayer() {
+        if (!_isUnityReady) {
+            return null;
+        }
+        return unityPlayer;
+    }
+
+    public static boolean isUnityReady() {
+        return _isUnityReady;
+    }
+
+    public static boolean isUnityPaused() {
+        return _isUnityPaused;
     }
 
     boolean keepAwake;
@@ -78,5 +103,130 @@ public class RNUnityModule extends ReactContextBaseJavaModule {
                 }
             });
         }
+    }
+
+    public static void createPlayer(final Activity activity, final UnityPlayerCallback callback) throws InvocationTargetException, NoSuchMethodException, IllegalAccessException {
+        if (unityPlayer != null) {
+            callback.onReady();
+
+            return;
+        }
+
+        if (activity != null) {
+            activity.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    activity.getWindow().setFormat(PixelFormat.RGBA_8888);
+                    int flag = activity.getWindow().getAttributes().flags;
+                    boolean fullScreen = false;
+                    if ((flag & WindowManager.LayoutParams.FLAG_FULLSCREEN) == WindowManager.LayoutParams.FLAG_FULLSCREEN) {
+                        fullScreen = true;
+                    }
+
+                    try {
+                        unityPlayer = new UPlayer(activity, callback);
+                    } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | InvocationTargetException e) {}
+
+                    try {
+                        // wait a moment. fix unity cannot start when startup.
+                        Thread.sleep(1000);
+                    } catch (Exception e) {}
+
+                    // start unity
+                    try {
+                        addUnityViewToBackground();
+                    } catch (InvocationTargetException | IllegalAccessException | NoSuchMethodException e) {}
+
+                    unityPlayer.windowFocusChanged(true);
+
+                    try {
+                        unityPlayer.requestFocusPlayer();
+                    } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {}
+
+                    unityPlayer.resume();
+
+                    if (!fullScreen) {
+                        activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+                        activity.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+                    }
+
+                    _isUnityReady = true;
+
+                    try {
+                        callback.onReady();
+                    } catch (InvocationTargetException | IllegalAccessException | NoSuchMethodException e) {}
+                }
+            });
+        }
+    }
+
+    public static void pause() {
+        if (unityPlayer != null) {
+            unityPlayer.pause();
+            _isUnityPaused = true;
+        }
+    }
+
+    public static void resume() {
+        if (unityPlayer != null) {
+            unityPlayer.resume();
+            _isUnityPaused = false;
+        }
+    }
+
+    public static void unload() {
+        if (unityPlayer != null) {
+            unityPlayer.unload();
+            _isUnityPaused = false;
+        }
+    }
+
+    public static void addUnityViewToBackground() throws InvocationTargetException, NoSuchMethodException, IllegalAccessException {
+        if (unityPlayer == null) {
+            return;
+        }
+
+        if (unityPlayer.getParentPlayer() != null) {
+            // NOTE: If we're being detached as part of the transition, make sure
+            // to explicitly finish the transition first, as it might still keep
+            // the view's parent around despite calling `removeView()` here. This
+            // prevents a crash on an `addContentView()` later on.
+            // Otherwise, if there's no transition, it's a no-op.
+            // See https://stackoverflow.com/a/58247331
+            ((ViewGroup) unityPlayer.getParentPlayer()).endViewTransition(unityPlayer.requestFrame());
+            ((ViewGroup) unityPlayer.getParentPlayer()).removeView(unityPlayer.requestFrame());
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            unityPlayer.setZ(-1f);
+        }
+
+        final Activity activity = ((Activity) unityPlayer.getContextPlayer());
+        ViewGroup.LayoutParams layoutParams = new ViewGroup.LayoutParams(1, 1);
+        activity.addContentView(unityPlayer.requestFrame(), layoutParams);
+    }
+
+    public static void addUnityViewToGroup(ViewGroup group) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        if (unityPlayer == null) {
+            return;
+        }
+
+        if (unityPlayer.getParentPlayer() != null) {
+            ((ViewGroup) unityPlayer.getParentPlayer()).removeView(unityPlayer.requestFrame());
+        }
+
+        ViewGroup.LayoutParams layoutParams = new ViewGroup.LayoutParams(MATCH_PARENT, MATCH_PARENT);
+        group.addView(unityPlayer.requestFrame(), 0, layoutParams);
+        unityPlayer.windowFocusChanged(true);
+        unityPlayer.requestFocusPlayer();
+        unityPlayer.resume();
+    }
+
+    public interface UnityPlayerCallback {
+        void onReady() throws InvocationTargetException, NoSuchMethodException, IllegalAccessException;
+
+        void onUnload();
+
+        void onQuit();
     }
 }
